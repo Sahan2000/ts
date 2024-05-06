d3ac9f550b6d11631cd4631ed8729cb1a4dc1a00
Sahan2000 <udanasahan1@gmail.com>
2024-05-06 13:44:49 +0530

    ExplicitTypes

---
+++ 02ExplicitTypes.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function isPass(total) {
+    return total >= 75 ? true : false;
+}
+isPass(57);
+isPass(75.5);
+/*
+isPass('hi');
+isPass(true);
+*/
+function greet(person, date) { }
+greet('Rayan', new Date());
+var x = 10;
+console.log(x);
+//x= 'hello';
+//type annotation
+var message = 'Hello! Good Morning';
+message = 'd';
---
+++ 02ExplicitTypes.ts
@@ -0,0 +1,27 @@
+function isPass(total : number){
+    return total >= 75 ? true : false;
+}
+isPass(57);
+isPass(75.5);
+
+/*
+isPass('hi');
+isPass(true);
+*/
+
+function greet(person : string, date : Date){}
+
+greet('Rayan', new Date());
+
+var x = 10;
+console.log(x);
+
+//x= 'hello';
+
+//type annotation
+let message : string = 'Hello! Good Morning';
+message = 'd';
+
+//message = 10;
+
+export{}
