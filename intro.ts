eb7993bc4835456dec3655217d421041a4148f33
Sahan2000 <udanasahan1@gmail.com>
2024-05-06 13:22:33 +0530

    Intro

---
+++ intro.ts
@@ -0,0 +1,23 @@
+let user = {
+    name : 'Jhon',
+    address : "USA"
+}
+
+console.log(user.name);
+console.log(user.address);
+
+const message = "Hello";
+message;
+
+
+function flipCoins(){
+    return Math.random;
+}
+
+function greet(person, date){
+    console.log(`Hello ${person}, today is ${date}`);
+}
+
+greet("John", "2024-05-03");
+
+export {}
