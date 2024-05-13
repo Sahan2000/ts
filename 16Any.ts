f17bbcdf9b160a9ff755bd3dc9f6986144121e75
Sahan2000 <udanasahan1@gmail.com>
2024-05-13 12:35:10 +0530

    add any ts file

---
+++ 16Any.ts
@@ -0,0 +1,15 @@
+let a = 10; // type inference
+console.log(a);
+
+// a = 'hello'; // Illegal
+// a = true; // Illegal
+
+// -----------------------------------------------
+
+let b: any = 10;
+console.log(b);
+
+b = 'Sahan';
+b = true;
+
+export default{}
