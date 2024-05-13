76c9e223b89e50bbb3dee5fbb8cf883524328acf
Sahan2000 <udanasahan1@gmail.com>
2024-05-13 09:32:12 +0530

    Add files'

---
+++ 04Arraye.ts
@@ -0,0 +1,24 @@
+let data = [10,20,30,'Hi'];
+console.log(data);
+
+data.push(40);
+console.log(data);
+
+// ------------------------------------
+
+//let marks: number[] = [10.5, 'Hi']; // illegal
+let marks: number[] = [10.5, 10]; // legal
+
+// ------------------------------------
+
+let stId: string[] = ['10', 'S001'];
+
+// ------------------------------------
+
+let name: Array<string> = ['John', 'Steve'];
+
+// ------------------------------------
+
+
+
+export{}
---
+++ 05Function.ts
@@ -0,0 +1,54 @@
+function greet(name: string){
+    console.log("hello " +name.toUpperCase());
+}
+
+greet("Matheve");
+
+// ------------------------------------
+
+// 2. return type annotation
+function getRandomNumber(): number {
+    return Math.random();
+    // return "Hello";
+    // return 10 > 5;
+}
+
+const data = (): number =>{
+    // return 'a';
+    return 10;
+}
+
+const data2 = (): number => 10 +10;
+
+// -------------------------------------
+
+// 03 Annonymous function
+const marks: number[] = [10,20,30,40,50];
+
+// please give me a callerback function
+
+marks.forEach(function(mark){
+    console.log(mark + '-');
+});
+
+console.log('-----------------------------------');
+
+marks.forEach((mark)=>{
+    console.log(mark);
+});
+
+// -----------------------------------------------
+
+// void type annotation
+function printInfo(): void {
+    let name ='Sahan';
+    let age = 24;
+
+    console.log(`My name is ${name}, and my age is ${age} `)
+
+    // return "10"; // you cannot, because this function is now has void type annotation
+}
+
+printInfo();
+
+export{}
