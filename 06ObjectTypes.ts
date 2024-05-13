cc50a3278c0f9c4450081919b9adc45f8b1ba28d
Sahan2000 <udanasahan1@gmail.com>
2024-05-13 10:04:28 +0530

    add 07 union.ts

---
+++ 06ObjectTypes.ts
@@ -0,0 +1,14 @@
+
+function printUser(user: {id:number; name:string}) {
+    console.log (`The user id is ${user.id} `);
+    console.log (`The user name is ${user.name} `);
+}
+
+const user = {
+    id: 1,
+    name: 'Steve'
+};
+
+printUser(user);
+
+export{}
---
+++ 07UnionTypes.ts
@@ -0,0 +1,50 @@
+function printId(id: number | string){
+    console.log(id);
+}
+
+printId(10);
+printId('Sahan');
+// printId(true); // cannot accept this argument type 
+
+// ----------------------------------------------------
+
+function displayId(id: number | string){
+    if(typeof id == 'string'){
+        console.log(id.toUpperCase());
+    }else{
+        console.log(id);
+    }
+}
+
+displayId("sahan");
+displayId(10);
+
+//-----------------------------------------------------
+
+function welcomePeople(names: string | string[]){
+    if(Array.isArray(names)){
+        names.map((name)=>{
+            console.log(`Hey! Welcome ${name}!`);
+        });
+    }else{
+        console.log(`Hey! Welcome lone traveler ${names}!`);
+    }
+}
+
+welcomePeople(['Sahan','Nimna','Kashmi','Thashmika','Lushan']);
+welcomePeople('Kashmi');
+
+// -----------------------------------------------------
+// let data: (number | boolean)[] = [10,20,true,false,'Hi']; // can't add string value
+let data: (number | boolean)[] = [10,20,true,false];
+
+// ------------------------------------------------------
+// strict union
+function windowStatus(status: "open"|"close"){
+    console.log(`The window is ${status}`);
+}
+
+windowStatus("close");
+windowStatus("open");
+
+export{}
