8cc871d88972f03770a3bf1d4111d41750dcd837
Sahan2000 <udanasahan1@gmail.com>
2024-05-13 11:29:53 +0530

    add interfaces ts file

---
+++ 09Interfaces.ts
@@ -0,0 +1,14 @@
+interface Employee{
+    empId: number;
+    email: string;
+    name: string;
+}
+
+function validateEmployee(emp: Employee){
+    //validate logic here
+}
+
+validateEmployee({empId:1, email:'sahan@gmail.com',name:'Sahan'});
+
+// validateEmployee({empId:2, email:'steve@gmail.com', nam:"john"}); // Illegal
+
